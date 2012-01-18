999ee228f3126f54e4ed864a2f12275a462b0794
Darren Slatten <darrenslatten@gmail.com>
2012-01-18 14:43:00 -0600

    added id mofoSerpData

--- title.js
+++ title.js
@@ -1,4 +1,5 @@
 javascript:void(function(){
+	if(document.getElementById('mofoSerpData')){document.getElementById('mofoSerpData').parentNode.removeChild(document.getElementById('mofoSerpData'));}
 	var clean=function(innerHTMLString){return innerHTMLString.replace(/<\/?[^>]+(>|$)/g,"").replace(/[ ]{2,}/g," ").replace(/(^\s*)|(\s*$)/g,"").replace(/&[A-Za-z0-9#]{2,8};/g,"&")};
 	var winW=1000,winH=600;
 
@@ -18,7 +19,7 @@ javascript:void(function(){
 
 	var div=document.createElement('div');
 	var mofoListItems=document.getElementById('rso').getElementsByTagName('li');
-	var message='<div style="border:1px solid #9eaeb0;height:' + (winH-383) + 'px;overflow-y:auto;"><table cellpadding="6" cellspacing="1" border="0" bgcolor="#9eaeb0" style="position:relative;top:-1px;left:-1px"><tr><th bgcolor="#f0f7f8">Jump</th><th bgcolor="#f0f7f8">#</th><th bgcolor="#f0f7f8" width="512">Title</th><th bgcolor="#f0f7f8">Chars</th><th bgcolor="#f0f7f8">Width</th></tr>';
+	var message='<div id="mofoSerpData" style="border:1px solid #9eaeb0;height:' + (winH-383) + 'px;overflow-y:auto;"><table cellpadding="6" cellspacing="1" border="0" bgcolor="#9eaeb0" style="position:relative;top:-1px;left:-1px"><tr><th bgcolor="#f0f7f8">Jump</th><th bgcolor="#f0f7f8">#</th><th bgcolor="#f0f7f8" width="512">Title</th><th bgcolor="#f0f7f8">Chars</th><th bgcolor="#f0f7f8">Width</th></tr>';
 	var r=0,i,max1,linkURL,linkText,blueLink,minChar=999,maxChar=0,minWidth=512,maxWidth=0,bgColor;
 	var jumpLink,minCharJumpLinkId='',maxCharJumpLinkId='',minWidthJumpLinkId='',maxWidthJumpLinkId='';
 
